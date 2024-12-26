939aff1cd9fb54679cc9bf0c46dab02d41ccc5ca
Azzam <azzam.manager@gmail.com>
2024-12-26 20:17:33 +0300

    chore(account): update app.d.ts

--- apps/account/src/app.d.ts
+++ apps/account/src/app.d.ts
@@ -5,21 +5,21 @@ import type { TagSlots } from "lucide-svelte/icons/tag"
 
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		interface Locals {
-			db: import("@acme/db").DrizzleD1Database<typeof import("@acme/db").schema>
-			session: import("@acme/auth").Session | null
-			api: import("@acme/rpc").EdenTreaty
-		}
-		// interface PageData {}
-		// interface PageState {}
-		interface Platform {
-			env: Env
-			cf: CfProperties
-			ctx: ExecutionContext
-		}
-	}
+    namespace App {
+        // interface Error {}
+        interface Locals {
+            db: import("@acme/db").DrizzleD1Database<typeof import("@acme/db").schema>
+            session: import("@acme/auth").Session | null
+            api: import("@acme/rpc").EdenTreaty
+        }
+        // interface PageData {}
+        // interface PageState {}
+        interface Platform {
+            env: Env
+            cf: CfProperties
+            ctx: ExecutionContext
+        }
+    }
 }
 
 export { };
