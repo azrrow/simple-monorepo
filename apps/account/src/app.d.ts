67b49b5c88a90c05997f6ea1b6974f17f68f84f6
Azzam <azzam.manager@gmail.com>
2024-12-26 19:44:04 +0300

    Chore(account): update app.d.ts

--- apps/account/src/app.d.ts
+++ apps/account/src/app.d.ts
@@ -1,13 +1,25 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
+// See https://svelte.dev/docs/kit/types#app
+
+import type { Task } from "@acme/db"
+import type { TagSlots } from "lucide-svelte/icons/tag"
+
 // for information about these interfaces
 declare global {
 	namespace App {
-        interface Platform {
-            env: Env
-            cf: CfProperties
-            ctx: ExecutionContext
-        }
-    }
+		// interface Error {}
+		interface Locals {
+			db: import("@acme/db").DrizzleD1Database<typeof import("@acme/db").schema>
+			session: import("@acme/auth").Session | null
+			api: import("@acme/rpc").EdenTreaty
+		}
+		// interface PageData {}
+		// interface PageState {}
+		interface Platform {
+			env: Env
+			cf: CfProperties
+			ctx: ExecutionContext
+		}
+	}
 }
 
-export {};
+export { };
